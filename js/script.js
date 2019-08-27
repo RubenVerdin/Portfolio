e2e98ebaabe76092771d698054002474389d0550
Ruben <ruben.verdin.tz@outlook.com>
2019-08-27 17:34:48 -0400

    Remove the contact me
    Git hub only support static HTML

--- js/script.js
+++ js/script.js
@@ -70,33 +70,33 @@ function showSlides(n) {
 
 // Make the site appear as you scroll
 
-// var animateHTML = function() {
-//   var elems;
-//   var windowHeight;
-//   function init() {
-//     elems = document.querySelectorAll('.hidden');
-//     console.log(elems);
-//     windowHeight = window.innerHeight;
-//     addEventHandlers();
-//     checkPosition();
-//   }
-//   function addEventHandlers() {
-//     window.addEventListener('scroll', checkPosition);
-//     window.addEventListener('resize', init);
-//   }
-//   function checkPosition() {
-//     for (var i = 0; i < elems.length; i++) {
-//       var positionFromTop = elems[i].getBoundingClientRect().top;
-//       if (positionFromTop - windowHeight <= 0) {
-//         elems[i].className = elems[i].className.replace(
-//           'hidden',
-//           'fade-in-element'
-//         );
-//       }
-//     }
-//   }
-//   return {
-//     init: init
-//   };
-// };
-// animateHTML().init();
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    console.log(elems);
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'fade-in-element'
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
