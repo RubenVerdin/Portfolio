3d4ccd982e8c78ad66483170b46b56997e3a5d37
Ruben <ruben.verdin.tz@outlook.com>
2019-08-27 15:14:07 -0400

    Changes to style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// FILTER OF THE PROJECTS
 filterSelection("all");
 function filterSelection(c) {
   var x, i;
@@ -31,21 +32,19 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-console.log(btnContainer);
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+//Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function(){
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
 
 // MODAL - GALLERY IMAGE CODE
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1;
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -56,19 +55,48 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  let slides = document.getElementsByClassName("mySlides");
+
   if (n > slides.length) {slideIndex = 1}
+
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+// Make the site appear as you scroll
+
+// var animateHTML = function() {
+//   var elems;
+//   var windowHeight;
+//   function init() {
+//     elems = document.querySelectorAll('.hidden');
+//     console.log(elems);
+//     windowHeight = window.innerHeight;
+//     addEventHandlers();
+//     checkPosition();
+//   }
+//   function addEventHandlers() {
+//     window.addEventListener('scroll', checkPosition);
+//     window.addEventListener('resize', init);
+//   }
+//   function checkPosition() {
+//     for (var i = 0; i < elems.length; i++) {
+//       var positionFromTop = elems[i].getBoundingClientRect().top;
+//       if (positionFromTop - windowHeight <= 0) {
+//         elems[i].className = elems[i].className.replace(
+//           'hidden',
+//           'fade-in-element'
+//         );
+//       }
+//     }
+//   }
+//   return {
+//     init: init
+//   };
+// };
+// animateHTML().init();
