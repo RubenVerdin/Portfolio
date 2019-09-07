9c9f682c8841c0272bc19477d61ec7684ee48ac3
Ruben <ruben.verdin.tz@outlook.com>
2019-09-07 14:43:54 -0400

    onView Fix

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ function filterSelection(c) {
   if (c == "all") c = "";
   
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show"); 
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    RemoveClass(x[i], "show"); 
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
-function w3AddClass(element, name) {
+function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -20,7 +20,7 @@ function w3AddClass(element, name) {
   }
 }
 
-function w3RemoveClass(element, name) {
+function RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -69,34 +69,19 @@ function showSlides(n) {
 }
 
 // Make the site appear as you scroll
-
-var animateHTML = function() {
-  var elems;
-  var windowHeight;
-  function init() {
-    elems = document.querySelectorAll('.hidden');
-    console.log(elems);
-    windowHeight = window.innerHeight;
-    addEventHandlers();
-    checkPosition();
-  }
-  function addEventHandlers() {
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  }
-  function checkPosition() {
-    for (var i = 0; i < elems.length; i++) {
-      var positionFromTop = elems[i].getBoundingClientRect().top;
-      if (positionFromTop - windowHeight <= 0) {
-        elems[i].className = elems[i].className.replace(
-          'hidden',
-          'fade-in-element'
-        );
-      }
+function animateOnSight(before, after) {
+  let elems = document.querySelectorAll('.' + before);
+  let windowHeight = window.innerHeight;
+  for (let i = 0; i < elems.length; i++) 
+  {
+    let positionFromTop = elems[i].getBoundingClientRect().top;
+    if (positionFromTop - windowHeight <= 0) 
+    {
+      elems[i].className = elems[i].className.replace(
+        before,
+        after
+      );
     }
   }
-  return {
-    init: init
-  };
-};
-animateHTML().init();
+
+}//animateOnSight
